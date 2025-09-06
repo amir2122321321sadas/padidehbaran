2ecfec0f9ac4246a8076c8dbd2f3b79672491adb
amirmohammad123pro <amirmohammadjafarizadeh430@gmail.com>
2025-09-06 12:22:31 +0330

    fix problem deploy

--- Modules/Frontend/resources/views/livewire/client/items/banner/banner-base.blade.php
+++ Modules/Frontend/resources/views/livewire/client/items/banner/banner-base.blade.php
@@ -1,7 +1,7 @@
 <div class="swiper single-swiper-slider -translate-y-5">
     <div class="swiper-wrapper">
         @foreach($mainBanners as $mainBanner)
-            <livewire:frontend::client.items.banner.banner-base.banner-items :$mainBanner/>
+            <livewire:frontend.client.items.banner.banner-base.banner-items :$mainBanner/>
         @endforeach
     </div>
 
