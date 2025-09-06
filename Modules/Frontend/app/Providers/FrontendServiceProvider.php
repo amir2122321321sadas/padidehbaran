77a4f64e1896fdd133788deb1068efcfc97fe245
amirmohammad123pro <amirmohammadjafarizadeh430@gmail.com>
2025-09-06 12:26:01 +0330

    fix problem deploy

--- Modules/Frontend/app/Providers/FrontendServiceProvider.php
+++ Modules/Frontend/app/Providers/FrontendServiceProvider.php
@@ -31,7 +31,7 @@ class FrontendServiceProvider extends ServiceProvider
         $this->registerViews();
         $this->loadMigrationsFrom(module_path($this->name, 'database/migrations'));
         Livewire::component('frontend.client.items.banner.banner-base', BannerBase::class);
-        Livewire::component('frontend.client.items.banner.banner-items', BannerItems::class);
+        Livewire::component('frontend.client.items.banner.banner-base.banner-items', BannerItems::class);
 
     }
 
